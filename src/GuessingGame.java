a17984433a11539a41ee3d6a62f9f87fa6337aba
BT_1N3_03 <byu1510@bths.edu>
2018-09-25 10:16:58 -0400

    as

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -38,16 +38,58 @@ public class GuessingGame {
         {
             double ans = Math.ceil(Math.random() * 100);
             System.out.println("Your range of numbers if from 1 to 100, Good luck");
+            int guess = 0;
+            while (guess != ans)
+            {
+                guess = input.nextInt ();
+                if (guess == ans) {
+                    System.out.println("That's right, Good job");
+                }
+                if (guess < ans) {
+                    System.out.println("That's not right, Try a little higher this time");
+                }
+                if (guess > ans) {
+                    System.out.println("That's not right, Try a little lower this time");
+                }
+            }
         }
         if (diff.equals(3))
         {
             double ans = Math.ceil(Math.random() * 1000);
             System.out.println("Your range of numbers if from 1 to 1000, Good luck");
+            int guess = 0;
+            while (guess != ans)
+            {
+                guess = input.nextInt ();
+                if (guess == ans) {
+                    System.out.println("That's right, Good job");
+                }
+                if (guess < ans) {
+                    System.out.println("That's not right, Try a little higher this time");
+                }
+                if (guess > ans) {
+                    System.out.println("That's not right, Try a little lower this time");
+                }
+            }
         }
         if (diff.equals(4))
         {
             double ans = Math.ceil(Math.random() * 10000);
             System.out.println("Your range of numbers if from 1 to 10000, Good luck");
+            int guess = 0;
+            while (guess != ans)
+            {
+                guess = input.nextInt ();
+                if (guess == ans) {
+                    System.out.println("That's right, Good job");
+                }
+                if (guess < ans) {
+                    System.out.println("That's not right, Try a little higher this time");
+                }
+                if (guess > ans) {
+                    System.out.println("That's not right, Try a little lower this time");
+                }
+            }
         }
     }
 
