3bf4e378db437458e628ce03313d90d97c2f982b
BT_1N3_03@BKTECH.LOCAL <BT_1N3_03@BKTECH.LOCAL>
2018-09-25 10:15:05 -0400

    Initial commit

---
+++ src/GuessingGame.java
@@ -0,0 +1,57 @@
+package BYu;
+
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    public static void GuessingGame()
+    {
+        System.out.println("What is your name?");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine ();
+        System.out.print("Hello" + name);
+        System.out.println("Welcome to the guessing game" + name + "Enter a number 1 to 4 to choose your difficulty");
+        System.out.println("The higher the number, the harder it is");
+        String diff = input.nextLine ();
+        System.out.println("You have picked" + diff);
+
+        if (diff.equals(1))
+        {
+            double ans = Math.ceil(Math.random() * 10);
+            System.out.println("Your range of numbers if from 1 to 10, Good luck");
+            int guess = 0;
+            while (guess != ans)
+            {
+                guess = input.nextInt ();
+                if (guess == ans) {
+                    System.out.println("That's right, Good job");
+                }
+                if (guess < ans) {
+                    System.out.println("That's not right, Try a little higher this time");
+                }
+                if (guess > ans) {
+                    System.out.println("That's not right, Try a little lower this time");
+                }
+            }
+        }
+        if (diff.equals(2))
+        {
+            double ans = Math.ceil(Math.random() * 100);
+            System.out.println("Your range of numbers if from 1 to 100, Good luck");
+        }
+        if (diff.equals(3))
+        {
+            double ans = Math.ceil(Math.random() * 1000);
+            System.out.println("Your range of numbers if from 1 to 1000, Good luck");
+        }
+        if (diff.equals(4))
+        {
+            double ans = Math.ceil(Math.random() * 10000);
+            System.out.println("Your range of numbers if from 1 to 10000, Good luck");
+        }
+    }
+
+
+
+
+}
---
+++ src/TestFile.java
@@ -0,0 +1,11 @@
+package BYu;
+
+
+public class TestFile
+
+{
+    public static void main(String args[])
+    {
+
+    }
+}
